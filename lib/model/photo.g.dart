7cd72886c83a93bb828ce30afbed9c1ed1c71bac
yejun_1220@naver.com <yejun_1220@naver.com>
2022-07-27 22:45:41 +0900

    Freezed

--- lib/model/photo.g.dart
+++ lib/model/photo.g.dart
@@ -6,14 +6,14 @@ part of 'photo.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Photo _$PhotoFromJson(Map<String, dynamic> json) => Photo(
-      json['id'] as int,
-      json['tags'] as String,
-      json['previewURL'] as String,
+_$_Photo _$$_PhotoFromJson(Map<String, dynamic> json) => _$_Photo(
+      id: json['id'] as int,
+      tags: json['tags'] as String,
+      previewURL: json['previewURL'] as String,
     );
 
-Map<String, dynamic> _$PhotoToJson(Photo instance) => <String, dynamic>{
+Map<String, dynamic> _$$_PhotoToJson(_$_Photo instance) => <String, dynamic>{
       'id': instance.id,
       'tags': instance.tags,
-      'previewURL': instance.previewUrl,
+      'previewURL': instance.previewURL,
     };
--- lib/ui/widget/photo_widget.dart
+++ lib/ui/widget/photo_widget.dart
@@ -12,7 +12,7 @@ class PhotoWidget extends StatelessWidget {
       decoration: BoxDecoration(
         borderRadius: const BorderRadius.all(Radius.circular(16.0)),
         // photo.previewURL의 값이 고정 값이 아니고 photo에 의해 런타임시에 정해지므로 const를 쓸 수 없다.
-        image: DecorationImage(fit: BoxFit.cover, image: NetworkImage(photo.previewUrl)),
+        image: DecorationImage(fit: BoxFit.cover, image: NetworkImage(photo.previewURL)),
       ),
     );
   }
