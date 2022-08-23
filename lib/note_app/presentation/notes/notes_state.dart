cbb468ca1d7c62733c87247e5981c53508534465
im-yejun <yejun_1220@naver.com>
2022-08-23 16:31:23 +0900

    정렬 기능 구현

--- lib/note_app/presentation/notes/notes_state.dart
+++ lib/note_app/presentation/notes/notes_state.dart
@@ -1,15 +1,14 @@
 
 import 'package:clean_architecture/note_app/domain/model/note.dart';
+import 'package:clean_architecture/note_app/domain/util/note_order.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'notes_state.freezed.dart';
-part 'notes_state.g.dart';
 
 @freezed
 class NotesState with _$NotesState {
   factory NotesState({
     required List<Note> notes,
+    required NoteOrder noteOrder,
 }) = _NotesState;
-
-  factory NotesState.fromJson(Map<String, dynamic> json) => _$NotesStateFromJson(json);
 }
--- lib/note_app/presentation/notes/notes_view_model.dart
+++ lib/note_app/presentation/notes/notes_view_model.dart
@@ -1,13 +1,19 @@
 import 'package:clean_architecture/note_app/domain/model/note.dart';
 import 'package:clean_architecture/note_app/domain/use_case/use_cases.dart';
+import 'package:clean_architecture/note_app/domain/util/note_order.dart';
+import 'package:clean_architecture/note_app/domain/util/order_type.dart';
 import 'package:clean_architecture/note_app/presentation/notes/notes_event.dart';
 import 'package:clean_architecture/note_app/presentation/notes/notes_state.dart';
 import 'package:flutter/material.dart';
 
-class NotesViewModel with ChangeNotifier{
+class NotesViewModel with ChangeNotifier {
   final UseCases useCases;
 
-  NotesState _state = NotesState(notes: []);
+  NotesState _state = NotesState(
+    notes: [],
+    noteOrder: const NoteOrder.date(OrderType.descending()),
+  );
+
   NotesState get state => _state;
 
   Note? _recentlyDeletedNote;
@@ -15,26 +21,28 @@ class NotesViewModel with ChangeNotifier{
   NotesViewModel(this.useCases) {
     _loadNotes();
   }
-  
+
   void onEvent(NotesEvent event) {
-    event.when(loadNotes: _loadNotes, deleteNote: _deleteNote, restoreNote: _restoreNote);
+    event.when(
+        loadNotes: _loadNotes,
+        deleteNote: _deleteNote,
+        restoreNote: _restoreNote);
   }
 
-  Future<void> _loadNotes () async {
-    List<Note> notes = await useCases.getNotes();
-    notes.sort();
+  Future<void> _loadNotes() async {
+    List<Note> notes = await useCases.getNotes(state.noteOrder);
     _state = _state.copyWith(notes: notes);
     notifyListeners();
   }
 
-  Future<void> _deleteNote (Note note) async {
+  Future<void> _deleteNote(Note note) async {
     await useCases.deleteNote(note);
     _recentlyDeletedNote = note;
     await _loadNotes();
   }
 
-  Future<void> _restoreNote (Note note) async {
-    if(_recentlyDeletedNote != null) {
+  Future<void> _restoreNote(Note note) async {
+    if (_recentlyDeletedNote != null) {
       await useCases.addNote(_recentlyDeletedNote!);
       _recentlyDeletedNote = null;
       _loadNotes();
