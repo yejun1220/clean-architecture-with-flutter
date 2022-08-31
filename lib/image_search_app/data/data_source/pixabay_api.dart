e0b56ad97cf9f2148d8025332dc79037e8c8e90f
im-yejun <yejun_1220@naver.com>
2022-08-31 13:45:20 +0900

    코드 정리

--- lib/image_search_app/data/data_source/pixabay_api.dart
+++ lib/image_search_app/data/data_source/pixabay_api.dart
@@ -4,12 +4,12 @@ import 'package:clean_architecture/image_search_app/data/data_source/result.dart
 import 'package:http/http.dart' as http;
 
 class PixabayApi {
+  static const baseUrl = 'https://pixabay.com/api/';
+  static const key = '28262654-10d1d88c92ffee267c6caee5e';
+
   final http.Client client;
 
   PixabayApi(this.client);
-
-  static const baseUrl = 'https://pixabay.com/api/';
-  static const key = '28262654-10d1d88c92ffee267c6caee5e';
 
   Future<Result<Iterable>> fetch(String query) async {
     try {
